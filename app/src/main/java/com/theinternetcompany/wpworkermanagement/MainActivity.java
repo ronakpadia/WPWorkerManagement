4a7fcf182c6ab57451c6aa6d47d2a4d516fe9df5
Ronak Padia <ronakpadia3@gmail.com>
2021-03-08 19:20:51 +0530

    Added Projects Table on mainactivity

--- app/src/main/java/com/theinternetcompany/wpworkermanagement/MainActivity.java
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/MainActivity.java
@@ -4,13 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class    MainActivity extends AppCompatActivity {
+
+    TableLayout projectTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        projectTable = findViewById(R.id.projectTable);
+        TableRow row = new TableRow(this);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(lp);
+        TextView text = new TextView(this);
+        text.setText("CHALU HAI");
+        row.addView(text);
+        row.addView(text);
+        row.addView(text);
+        row.addView(text);
+        row.addView(text);
+        projectTable.addView(row);
+        projectTable.addView(row);
+//        transitionToNewWorkerActivity();
+    }
+
+    private void transitionToNewWorkerActivity() {
 
         Intent intent = new Intent(MainActivity.this, EditWorkerActivity.class);
         startActivity(intent);
