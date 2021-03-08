965e5c13341e55b57b143c5accaa487746cd704d
darshanpadia <darshan.padia@somaiya.edu>
2021-03-08 19:11:41 +0530

    WorkerDetail, EditWorker Activity Update

--- app/src/main/java/com/theinternetcompany/wpworkermanagement/EditWorkerActivity.java
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/EditWorkerActivity.java
@@ -9,49 +9,45 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.theinternetcompany.wpworkermanagement.Models.WorkerProfile;
 
-public class NewWorkerActivity extends AppCompatActivity {
+public class EditWorkerActivity extends AppCompatActivity {
 
     private EditText name, cardNo, workType, rate;
-    private Button btnCreateProfile;
-    private long back_pressed;
+    private Button btnSave;
     private DatabaseReference mainRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_worker);
+        setContentView(R.layout.activity_edit_worker);
 
-        mainRef.keepSynced(true);
-        name = findViewById(R.id.newName);
-        cardNo = findViewById(R.id.newCardNo);
-        workType = findViewById(R.id.newWorkType);
-        rate = findViewById(R.id.newRate);
-        btnCreateProfile = findViewById(R.id.btnCreateProfile);
+        name = findViewById(R.id.edtName);
+        cardNo = findViewById(R.id.edtCardNo);
+        workType = findViewById(R.id.edtWorkType);
+        rate = findViewById(R.id.edtRate);
+        btnSave = findViewById(R.id.btnSave);
 
-
-        btnCreateProfile.setOnClickListener(new View.OnClickListener() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (TextUtils.isEmpty(name.getText().toString())) {
-                    Toast.makeText(NewWorkerActivity.this,"Enter Worker Name", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditWorkerActivity.this,"Enter Worker Name", Toast.LENGTH_SHORT).show();
                 }
 
                 else if (TextUtils.isEmpty(cardNo.getText().toString())) {
-                    Toast.makeText(NewWorkerActivity.this,"Enter Card Number", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditWorkerActivity.this,"Enter Card Number", Toast.LENGTH_SHORT).show();
                 }
 
                 else if (TextUtils.isEmpty(workType.getText().toString())) {
-                    Toast.makeText(NewWorkerActivity.this,"Enter Work Type", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditWorkerActivity.this,"Enter Work Type", Toast.LENGTH_SHORT).show();
                 }
 
                 else if (TextUtils.isEmpty(rate.getText().toString())) {
-                    Toast.makeText(NewWorkerActivity.this,"Enter Rate", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditWorkerActivity.this,"Enter Rate", Toast.LENGTH_SHORT).show();
                 }
 
                 else {
@@ -61,14 +57,13 @@ public class NewWorkerActivity extends AppCompatActivity {
             }
 
         });
-
     }
 
     private void saveWorkerProfileData()
     {
-        final String workerID = mainRef.child("Worker_List").push().getKey();
-        WorkerProfile newWorker = new WorkerProfile(workerID, name.getText().toString(), cardNo.getText().toString(), rate.getText().toString(), workType.getText().toString());
-        mainRef.child("Worker_List").child(workerID).setValue(newWorker);
+
+        WorkerProfile newWorker = new WorkerProfile("-MVGdE48egxQm3MXHiFB", name.getText().toString(), cardNo.getText().toString(), workType.getText().toString(), rate.getText().toString() );
+        mainRef.child("Worker_List").child("-MVGdE48egxQm3MXHiFB").setValue(newWorker);
 //        final String workerID = FirebaseDatabase.getInstance().getReference().child("Worker_List").push().getKey();
 //        WorkerProfile newWorker = new WorkerProfile(workerID, name.getText().toString(), cardNo.getText().toString(), rate.getText().toString(), workType.getText().toString());
 //        FirebaseDatabase.getInstance().getReference().child("Worker_List").child(workerID).setValue(newWorker);
--- app/src/main/java/com/theinternetcompany/wpworkermanagement/WokerDetailActivity.java
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/WokerDetailActivity.java
@@ -2,17 +2,13 @@ package com.theinternetcompany.wpworkermanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class    MainActivity extends AppCompatActivity {
+public class WokerDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(MainActivity.this, EditWorkerActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_woker_detail);
     }
 }
