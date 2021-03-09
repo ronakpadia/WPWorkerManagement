61467862150e1444192aaf1762e87ee8aea98d98
Ronak Padia <ronakpadia3@gmail.com>
2021-03-09 18:45:10 +0530

    DIALOG UNFINISHED CODE
    Unfinished code that will break on mainactivity.java when clicked on the eye icon on top right corner due to setonclicklistner on null object

--- app/src/main/java/com/theinternetcompany/wpworkermanagement/MainActivity.java
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -22,10 +25,11 @@ import com.theinternetcompany.wpworkermanagement.Models.Project;
 
 import java.util.ArrayList;
 
-public class    MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     TableLayout projectTable;
-    private Button btnEmployeeList, btnAddNewProject;
+    private Button btnEmployeeList, btnAddNewProject, btnHideShowColumns;
+    private TextView idTag, nameTag, locationTag, companyTag, expensesTag;
     private DatabaseReference mainRef = FirebaseDatabase.getInstance().getReference();
     private ArrayList<Project> projectList = new ArrayList<>();
 
@@ -42,6 +46,12 @@ public class    MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnAddNewProject = findViewById(R.id.btnAddNewProject);
         btnEmployeeList = findViewById(R.id.btnEmployeeList);
+        btnHideShowColumns = findViewById(R.id.btnHideShowColumns);
+        idTag = findViewById(R.id.idTag);
+        nameTag = findViewById(R.id.nameTag);
+        locationTag = findViewById(R.id.locationTag);
+        companyTag = findViewById(R.id.companyTag);
+        expensesTag = findViewById(R.id.expensesTag);
         mainRef.keepSynced(true);
         getProjectData(); // Populate the table with latest project
         btnAddNewProject.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +60,37 @@ public class    MainActivity extends AppCompatActivity {
                 transitionToNewProjectActivity();
             }
         });
+        btnHideShowColumns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.dialog_hide_show_columns, viewGroup, false);
+                builder.setView(dialogView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                Button btnIdHideShow;
+                btnIdHideShow = findViewById(R.id.btnViewColID);
+                btnIdHideShow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        collapseId();
+                    }
+                });
+            }
+        });
+        idTag.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                collapseId();
+            }
+        });
 //        transitionToNewWorkerActivity();
+    }
+
+    private void collapseId() {
+        projectTable = findViewById(R.id.projectTable);
+        projectTable.setColumnCollapsed(0, true);
     }
 
     private void getProjectData() {
