2ae9a214e2a81889285a438f35ad3d460255a080
darshanpadia <darshan.padia@somaiya.edu>
2021-03-12 07:55:40 +0530

    added WorkerDetailActivity
    + added tableLayout in activity_worker_detail

---
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/WorkerListActivity.java
@@ -0,0 +1,14 @@
+package com.theinternetcompany.wpworkermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_list);
+    }
+}
