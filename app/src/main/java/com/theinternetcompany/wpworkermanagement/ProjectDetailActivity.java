ad0d3060e2e61460a62ab9a0befe0a6af884f043
darshanpadia <darshan.padia@somaiya.edu>
2021-03-12 12:09:55 +0530

    ProjectDetailActivity Created

---
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/ProjectDetailActivity.java
@@ -0,0 +1,14 @@
+package com.theinternetcompany.wpworkermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_detail);
+    }
+}
