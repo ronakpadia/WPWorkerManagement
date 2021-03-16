997a3ce02dff186cd51ef5797d98cc223fd78ad3
darshanpadia <darshan.padia@somaiya.edu>
2021-03-16 15:17:35 +0530

    incomplete add, remove worker in project

--- app/src/main/java/com/theinternetcompany/wpworkermanagement/ProjectDetailActivity.java
+++ app/src/main/java/com/theinternetcompany/wpworkermanagement/ProjectDetailActivity.java
@@ -6,8 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -23,9 +30,14 @@ import java.util.ArrayList;
 public class ProjectDetailActivity extends AppCompatActivity {
 
     private Project project = new Project();
+    TableLayout projectTable, projectTable2;
+    private SearchView searchView;
     private ArrayList<WorkerProfile> workerList = new ArrayList<>();
+    private ArrayList<WorkerProfile> pWorkerList = new ArrayList<>();
     private DatabaseReference mainRef = FirebaseDatabase.getInstance().getReference();
     private TextView projectName, projectId, projectLocation, projectCompany, projectDuration;
+    private Button btnAddWorker;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +45,14 @@ public class ProjectDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_project_detail);
         projectName = findViewById(R.id.twProjectName);
         projectId = findViewById(R.id.twProjectId);
+        projectTable = findViewById(R.id.projectTable);
+        projectTable2 = findViewById(R.id.projectTable2);
+        searchView = findViewById(R.id.searchView);
+        layout = findViewById(R.id.linearLayoutT);
         projectCompany = findViewById(R.id.twProjectCompany);
         projectDuration = findViewById(R.id.twProjectDuration);
         projectLocation = findViewById(R.id.twProjectLocation);
+        btnAddWorker = findViewById(R.id.btnAddWorker);
 
         Intent i = getIntent();
         project = (Project) i.getSerializableExtra("project");
@@ -44,7 +61,18 @@ public class ProjectDetailActivity extends AppCompatActivity {
         projectCompany.setText(project.getCompany());
         projectLocation.setText(project.getLocation());
         projectDuration.setText(project.getPeriod());
+        layout.setVisibility(View.GONE);
         getWorkerData();
+
+        btnAddWorker.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                btnAddWorker.setVisibility(View.GONE);
+                layout .setVisibility(View.VISIBLE);
+
+            }
+        });
 
     }
 
@@ -60,8 +88,8 @@ public class ProjectDetailActivity extends AppCompatActivity {
                     workerList.add(snap.getValue(WorkerProfile.class));
 
                 }
+                populateTable(workerList, projectTable2);
 
-                setPedictions();
 
             }
 
@@ -72,14 +100,89 @@ public class ProjectDetailActivity extends AppCompatActivity {
         });
     }
 
-    private void setPedictions() {
-        ArrayAdapter<WorkerProfile> adapter = new ArrayAdapter<WorkerProfile>(this,
-                android.R.layout.select_dialog_item,
-                workerList);
-        //Getting the instance of AutoCompleteTextView
-        AutoCompleteTextView actv =  (AutoCompleteTextView)findViewById(R.id.addWorkerAutoComplete);
-        actv.setThreshold(1);//will start working from first character
-        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
-        actv.setTextColor(Color.RED);
+    private void getPWorkerData() {
+        DatabaseReference workerRef = mainRef.child("Project_List").child("worker_list");
+        workerRef.keepSynced(true);
+
+
+        workerRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snap : snapshot.getChildren()){
+                    workerList.add(snap.getValue(WorkerProfile.class));
+
+                }
+                populateTable(workerList, projectTable2);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+    private void populateTable(ArrayList<WorkerProfile> workerList, TableLayout table) {
+        cleanTable(table);
+        for (WorkerProfile p : workerList){
+            TableRow row = new TableRow(ProjectDetailActivity.this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+            row.setLayoutParams(lp);
+            // Generating TextViews for row
+//            TextView pid = new TextView(MainActivity.this);
+//            pid.setText(p.getId());
+            TextView wName = new TextView(ProjectDetailActivity.this);
+            wName.setText(p.getName());
+            wName.setPadding(20,20,20,20);
+            wName.setTextSize(20);
+            TextView card = new TextView(ProjectDetailActivity.this);
+            card.setText(p.getCardNo());
+            card.setPadding(20,20,20,20);
+            card.setTextSize(20);
+            TextView rate = new TextView(ProjectDetailActivity.this);
+            rate.setText(p.getRate());
+            rate.setPadding(20,20,20,20);
+            rate.setTextSize(20);
+            TextView workType = new TextView(ProjectDetailActivity.this);
+            workType.setText(p.getWorkType());
+            workType.setPadding(20,20,20,20);
+            workType.setTextSize(20);
+            // Adding TextViews
+//            row.addView(pid);
+            row.addView(wName);
+            row.addView(card);
+            row.addView(rate);
+            row.addView(workType);
+            // Adding the row to tableLayout
+            Log.v("BHENCHOD", "MADARCHOD");
+
+            row.setClickable(true);
+            row.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Log.v("Click", p.getName());
+                    pWorkerList.add(p);
+                    populateTable(pWorkerList, projectTable);
+
+                }
+            });
+            table.addView(row);
+            Log.v("KIA ADD", "RANDI");
+        }
+    }
+
+    private void cleanTable(TableLayout table) {
+
+        int childCount = table.getChildCount();
+
+        // Remove all rows except the first one
+        if (childCount > 1) {
+            table.removeViews(1, childCount - 1);
+        }
+    }
+
+
 }
